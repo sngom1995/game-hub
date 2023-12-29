a48a8fc725f582d904d8cfc04dd9a419e53be2ef
Samba Ngom <ngomsamba75@gmail.com>
2023-12-29 10:32:48 +0000

    Add NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
 import { useState } from "react";
 import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import reactLogo from "./assets/react.svg";
+import NavBar from "./components/NavBar";
 import "./App.css";
 
 function App() {
   return (
-    <ButtonGroup>
-      <Button colorScheme="blue">Button 1</Button>
-      <Button colorScheme="teal">Button 2</Button>
-      <Button colorScheme="pink">Button 3</Button>
-    </ButtonGroup>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar />
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
   );
 }
 
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { HStack, Image } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
+
+const NavBar = () => {
+  return (
+    <HStack>
+      <Image src={logo} alt="" boxSize="60px" />
+    </HStack>
+  );
+};
+
+export default NavBar;
