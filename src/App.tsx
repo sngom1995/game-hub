f47e96b1fbc0db3e59a2a12e7680bc1602eff42e
Samba Ngom <ngomsamba75@gmail.com>
2024-01-03 19:48:05 +0000

    add dynamic heading

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
 import { Platform } from "./hooks/usePlatform";
 import SortSelector from "./components/SortSelector";
+import GameHeading from "./components/GameHeading";
 
 export interface GameQuery {
   genre: Genre | null;
@@ -46,22 +47,25 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area="main">
-        <Flex paddingLeft={2} marginBottom={5}>
-          <Box marginRight={5}>
-            <PlatformSelector
-              selectedPlatform={gameQuery.platform!}
-              onSelectPlatform={(platform) =>
-                setGameQuery({ ...gameQuery, platform: platform })
+        <Box paddingLeft={2}>
+          <GameHeading gameQuery={gameQuery} />
+          <Flex marginBottom={5}>
+            <Box marginRight={5}>
+              <PlatformSelector
+                selectedPlatform={gameQuery.platform!}
+                onSelectPlatform={(platform) =>
+                  setGameQuery({ ...gameQuery, platform: platform })
+                }
+              />
+            </Box>
+            <SortSelector
+              onSort={(sortKey) =>
+                setGameQuery({ ...gameQuery, sortOrder: sortKey })
               }
+              sortKey={gameQuery.sortOrder}
             />
-          </Box>
-          <SortSelector
-            onSort={(sortKey) =>
-              setGameQuery({ ...gameQuery, sortOrder: sortKey })
-            }
-            sortKey={gameQuery.sortOrder}
-          />
-        </Flex>
+          </Flex>
+        </Box>
         <GameGrid gameQuery={gameQuery} />
       </GridItem>
     </Grid>
